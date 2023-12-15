feat(train): add number_of_weights fix(trainer): fix dumb metric calculation fix(model): fix gru_hidden_size, sinc_kernel size and add fc layer fix(utils): fix SwitchABS off case fix: imports

src/metric/eer_metric.py
@@ -1,4 +1,4 @@
-from typing import List
+import torch
 
 from .base_metric import BaseMetric
 from .utils import compute_eer
@@ -6,13 +6,25 @@ from .utils import compute_eer
 
 class EERMetric(BaseMetric):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, epoch_based=True, **kwargs)
+        self.logits = []
+        self.targets = []
 
     def __call__(self, logits, target, **kwargs):
+        self.logits.append(logits)
+        self.targets.append(target)
+
+        return None
+
+    def calculate(self):
+        logits = torch.cat(self.logits)
+        targets = torch.cat(self.targets)
+
         true_scores = logits.detach().cpu().numpy()[..., 1]
-        target = target.detach().cpu().numpy()
+        target = targets.detach().cpu().numpy()
 
-        print(f"{logits.shape=}")
-        print(f"{target.shape=}")
+        ans = compute_eer(true_scores[target == 1], true_scores[target == 0])[0]
+        self.logits = []
+        self.targets = []
 
-        return compute_eer(true_scores[target == 1], true_scores[target == 0])[0]
+        return ans

src/model/rawnet2/rawnet2.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 from .reslayer import ResLayer
@@ -15,18 +16,20 @@ class RawNet2(nn.Module):
         self,
         gru_prenorm=True,
         gru_num_layers=3,
+        gru_hidden_size=1024,
         reslayer1_out_channels=20,
         reslayer2_out_channels=128,
         sinc_abs_after=True,
         sinc_min_low_hz=0,
         sinc_min_band_hz=0,
         sinc_requires_grad=False,
+        sinc_kernel_size=1024,
     ):
         super().__init__()
         self.net = nn.Sequential(
             SincConv_fast(
                 out_channels=128,
-                kernel_size=129,
+                kernel_size=sinc_kernel_size,
                 min_low_hz=sinc_min_low_hz,
                 min_band_hz=sinc_min_band_hz,
                 requires_grad=sinc_requires_grad,
@@ -52,15 +55,19 @@ class RawNet2(nn.Module):
         self.gru = nn.GRU(
             batch_first=True,
             num_layers=gru_num_layers,
-            hidden_size=reslayer2_out_channels * 2,
+            hidden_size=gru_hidden_size,
             input_size=reslayer2_out_channels,
         )
-        self.fc = nn.Linear(reslayer2_out_channels * 2, 2)
+        self.fc = nn.Linear(gru_hidden_size, gru_hidden_size)
+        self.fc_out = nn.Linear(gru_hidden_size, 2)
 
     def forward(self, audio, **kwargs):
         x = audio.unsqueeze(1)
         x = self.net(x)
         x = self.gru(x.transpose(-1, -2))[0][:, -1, :]
         x = self.fc(x)
+        norm = x.norm(p=2, dim=1, keepdim=True) / 10.0
+        x = torch.div(x, norm)
+        x = self.fc_out(self.fc(x))
 
         return x

src/model/rawnet2/utils.py
@@ -30,4 +30,4 @@ class SwitchABS(nn.Module):
         if self.is_on:
             return torch.abs(x)
         else:
-            x
+            return x

src/trainer/trainer.py
@@ -1,17 +1,11 @@
-import random
 from pathlib import Path
 from random import shuffle
 
 import pandas as pd
-import PIL
 import torch
-import torch.nn.functional as F
 from torch.nn.utils import clip_grad_norm_
-from torchvision.transforms import ToTensor
 from tqdm import tqdm
 
-from src.logger.utils import plot_spectrogram_to_buf
-from src.metric import EERMetric
 from src.utils import MetricTracker, inf_loop
 
 from .base_trainer import BaseTrainer
@@ -185,18 +179,12 @@ class Trainer(BaseTrainer):
             with torch.no_grad():
                 metrics.update("loss", batch["loss"].item())
                 for met in self.metrics["shared"]:
-                    if isinstance(met, EERMetric):
-                        continue
                     metrics.update(met.name, met(**batch))
                 if is_train:
                     for met in self.metrics["train"]:
-                        if isinstance(met, EERMetric):
-                            continue
                         metrics.update(met.name, met(**batch))
                 else:
                     for met in self.metrics["evaluation"]:
-                        if isinstance(met, EERMetric):
-                            continue
                         metrics.update(met.name, met(**batch))
         return batch
 
@@ -209,8 +197,6 @@ class Trainer(BaseTrainer):
         """
         self.model.eval()
         self.evaluation_metrics.reset()
-        logits = []
-        targets = []
         with torch.no_grad():
             for batch_idx, batch in tqdm(
                 enumerate(dataloader),
@@ -223,17 +209,11 @@ class Trainer(BaseTrainer):
                     is_train=False,
                     metrics=self.evaluation_metrics,
                 )
-                logits.append(batch["logits"])
-                targets.append(batch["target"])
-            logits = torch.cat(logits)
-            targets = torch.cat(targets)
 
             # TODO: fix that dumb logic
             for met in self.metrics["evaluation"]:
-                if isinstance(met, EERMetric):
-                    self.evaluation_metrics.update(
-                        met.name, met(target=targets, logits=logits)
-                    )
+                if met.epoch_based:
+                    self.evaluation_metrics.update(met.name, met.calculate())
 
             self.writer.set_step(epoch * self.len_epoch, part)
             self._log_scalars(self.evaluation_metrics)

train.py
@@ -3,7 +3,6 @@ import os
 import sys
 import warnings
 from pathlib import Path
-from string import ascii_lowercase
 
 import hydra
 import numpy as np
@@ -27,6 +26,10 @@ torch.backends.cudnn.benchmark = False
 np.random.seed(SEED)
 
 
+def number_of_weights(nn):
+    return sum(p.numel() for p in nn.parameters() if p.requires_grad)
+
+
 @hydra.main(version_base=None, config_path="src/config", config_name="train")
 def main(cfg: DictConfig):
     OmegaConf.resolve(cfg)
@@ -47,6 +50,7 @@ def main(cfg: DictConfig):
     dataloaders = get_dataloaders(cfg)
 
     model = instantiate(cfg["arch"])
+    print(f"{number_of_weights(model)=}")
     logger.info(model)
 
     device, device_ids = prepare_device(cfg["n_gpu"])