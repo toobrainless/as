ed4070eea5ad69e40068171ead5f64a2424d0873
toobrainless <toobrainless@gmail.com>
2023-12-16 18:07:00 +0300

    feat!: add test.py feat: add test_audio

--- test.py
+++ test.py
@@ -1,101 +1,71 @@
-import json
-import logging
-import os
-import sys
+import math
 from pathlib import Path
-from string import ascii_lowercase
 
-import hydra
+import click
+import pandas as pd
 import torch
-from hydra.utils import get_original_cwd, instantiate, to_absolute_path
-from omegaconf import DictConfig, OmegaConf
-from tqdm import tqdm
-
-from src.trainer import Trainer
-from src.utils import MetricTracker
-from src.utils.object_loading import get_dataloaders
-from src.utils.util import ROOT_PATH
+import torchaudio
+from hydra import compose, initialize
+from hydra.utils import instantiate
+from omegaconf import OmegaConf
+from torch.nn import functional as F
 
 
-@hydra.main(version_base=None, config_path="hw_asr/config", config_name="test")
-def main(test_cfg: DictConfig):
-    print(f"{get_original_cwd()=}")
-    OmegaConf.resolve(test_cfg)
-    print(OmegaConf.to_yaml(test_cfg))
+def load_audio(path):
+    OUTPUT_SAMPLES = 64000
+    audio = torchaudio.load(path)[0]
+    repeats = math.ceil(OUTPUT_SAMPLES / audio.shape[1])
+    audio = audio.repeat((1, repeats))[:, :OUTPUT_SAMPLES]
 
-    model_ckpt_path = Path(to_absolute_path(ROOT_PATH / test_cfg["checkpoint_path"]))
-    print(f"{model_ckpt_path=}")
-    model_working_dir = model_ckpt_path.parent
-    model_cfg = OmegaConf.load(model_working_dir / ".hydra/config.yaml")
-    OmegaConf.resolve(model_cfg)
-    print(OmegaConf.to_yaml(model_cfg))
+    return audio
+    return audio
 
-    logging.basicConfig(filename="test.log")
-    logger = logging.getLogger("test")
-    logger.setLevel(logging.INFO)
 
-    # define cpu or gpu if possible
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+@click.command()
+@click.option(
+    "--model_path",
+    "-mp",
+    default="small",
+    help="Path to folder with model.",
+    type=str,
+)
+@click.option(
+    "--test_audio_path",
+    "-tap",
+    default="test_audio",
+    help="Path to folder with test audio.",
+    type=str,
+)
+def main(model_path, test_audio_path):
+    model_path = Path(model_path)
+    audio_batch = []
+    audio_stems = []
 
-    # text_encoder
-    alphabet = list(ascii_lowercase + " ")
-    text_encoder = instantiate(model_cfg.text_encoder, alphabet=alphabet)
+    test_audio_path = Path(test_audio_path)
+    for path in sorted(test_audio_path.iterdir()):
+        audio_stems.append(path.stem)
+        audio_batch.append(load_audio(path))
 
-    # setup data_loader instances
-    dataloaders = get_dataloaders(test_cfg, text_encoder)
+    audio_batch = torch.concatenate(audio_batch)
 
-    # build model architecture
-    model = instantiate(model_cfg["arch"], n_class=len(text_encoder))
-    logger.info(model)
+    with initialize(version_base=None, config_path=model_path):
+        cfg = compose(config_name="config")
+        OmegaConf.resolve(cfg)
 
-    logger.info("Loading checkpoint: {} ...".format(test_cfg["checkpoint_path"]))
-    checkpoint = torch.load(model_ckpt_path, map_location=device)
-    state_dict = checkpoint["state_dict"]
-    if test_cfg["n_gpu"] > 1:
-        model = torch.nn.DataParallel(model)
-    model.load_state_dict(state_dict)
+    model = instantiate(cfg["arch"])
+    model.load_state_dict(torch.load(model_path / "checkpoint.pth")["state_dict"])
 
-    # prepare model for testing
-    model = model.to(device)
-    model.eval()
+    logits = model(audio=audio_batch)
+    probabilities = F.softmax(logits, dim=1).tolist()
 
-    metrics = [
-        instantiate(metric, text_encoder=text_encoder) for metric in test_cfg["metrics"]
-    ]
-    tracker = MetricTracker(*[m for m_list in metrics for m in m_list.get_metrics()])
-    with torch.no_grad():
-        for name, dataloader in dataloaders.items():
-            print(name)
-            for batch_num, batch in enumerate(tqdm(dataloader)):
-                batch = Trainer.move_batch_to_device(batch, device)
-                output = model(**batch)
+    results = pd.DataFrame(columns=["stem", "spoof_prob", "bonified_prob"])
 
-                if type(output) is dict:
-                    batch.update(output)
-                else:
-                    batch["logits"] = output
-                batch["log_probs"] = torch.log_softmax(batch["logits"], dim=-1)
-                batch["log_probs_length"] = model.transform_input_lengths(
-                    batch["spectrogram_length"]
-                )
-                batch["probs"] = batch["log_probs"].exp().cpu()
-                batch["argmax"] = batch["probs"].argmax(-1)
-                for met in metrics:
-                    tracker.update(met.name, met(**batch))
+    for idx, (stem, probability) in enumerate(zip(audio_stems, probabilities)):
+        results.loc[idx] = [stem, *list(map(lambda x: f"{x:.7f}", probability))]
 
-            print(f"{name} -- done")
-            print("Results:")
-            print(tracker.result())
-
-            with open(f"{name}_results.json", "w") as f:
-                json.dump(tracker.result(), f, indent=2)
-            tracker.reset()
+    results.to_csv(f"{model_path.stem}_results.csv")
+    print(results)
 
 
 if __name__ == "__main__":
-    logger = logging.getLogger("test")
-    logger.setLevel(logging.INFO)
-    logger.info("3.1415926")
-    sys.argv.append("hydra.job.chdir=True")
-    os.environ["HYDRA_FULL_ERROR"] = "1"
     main()
